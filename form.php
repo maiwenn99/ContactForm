4f334d2bcf50552b08f78a05813661eb0d899e93
maiwenn99 <maiwenngay99@gmail.com>
2019-03-17 23:40:19 +0100

    finished contactform and added var_filter

--- form.php
+++ form.php
@@ -6,13 +6,13 @@ if ($_POST) {
     }
 
 
-
     if (empty($_POST['prenom'])) {
         $errors['prenom1'] = "Veuillez renseigner votre prénom";
     }
 
-    if (empty($_POST['courriel'])) {
-        $errors['courriel1'] = "Veuillez renseigner votre adresse e-mail";
+    $isEmailValid = filter_var($_POST['courriel'], FILTER_VALIDATE_EMAIL);
+    if ($isEmailValid === false) {
+        $errors['courriel1'] = "Veuillez renseigner une adresse e-mail valide";
     }
 
 
@@ -22,8 +22,7 @@ if ($_POST) {
 
 
     if (0 == count($errors)) {
-        echo "Votre message à été envoyez avec succès !";
-        $_POST = "";
+        unset($_POST);
     }
 
 
@@ -44,10 +43,19 @@ if ($_POST) {
 <body>
 
 <h1 class="text-center">Contactez-Nous</h1>
-<form action="" method="post">
+<form action="" method="post" novalidate>
+    <?php
+    if (isset($errors) && count($errors) == 0) {
+        ?>
+        <div class="centered">
+            Votre message a été envoyé.
+        </div>
+        <?php
+    }
+    ?>
     <div>
         <label for="nom">Nom :</label>
-        <input type="text" id="nom" name="nom" value="<?php if(isset($_POST['nom'])) echo $_POST['nom'];?>"/>
+        <input type="text" id="nom" name="nom" value="<?php if (isset($_POST['nom'])) echo $_POST['nom']; ?>"/>
         <p><?php if (isset($errors['nom1'])) echo $errors['nom1'];
             ?></p>
     </div>
@@ -55,7 +63,8 @@ if ($_POST) {
 
     <div>
         <label for="prenom">Prénom :</label>
-        <input type="text" id="prenom" name="prenom" value="<?php if(isset($_POST['prenom'])) echo $_POST['prenom'];?>"/>
+        <input type="text" id="prenom" name="prenom"
+               value="<?php if (isset($_POST['prenom'])) echo $_POST['prenom']; ?>"/>
         <p><?php if (isset($errors['prenom1'])) echo $errors['prenom1'];
             ?></p>
 
@@ -63,7 +72,8 @@ if ($_POST) {
 
     <div>
         <label for="courriel">Courriel :</label>
-        <input type="email" id="courriel" name="courriel" value="<?php if(isset($_POST['email'])) echo $_POST['email'];?> "/>
+        <input type="email" id="courriel" name="courriel"
+               value="<?php if (isset($_POST['email'])) echo $_POST['courriel']; ?> "/>
         <p><?php if (isset($errors['courriel1'])) echo $errors['courriel1'];
             ?></p>
 
@@ -71,15 +81,15 @@ if ($_POST) {
 
     <div>
         <label for="tel">Téléphone:</label>
-        <input type="tel" pattern="\d+" id="tel" name="tel" value="<?php if(isset($_POST['tel'])) echo $_POST['tel'];?>"/>
+        <input type="tel" pattern="\d+" id="tel" name="tel"
+               value="<?php if (isset($_POST['tel'])) echo $_POST['tel']; ?>"/>
         <p><?php if (isset($errors['tel1'])) echo $errors['tel1'];
             ?></p>
     </div>
 
 
-
     <div>
-        <label for="sujet">Objet:</label>
+        <label for="choix">Objet:</label>
         <select name="choix">
             <option value="choix0">Acheter un MacBook</option>
             <option value="choix1">Manger des Pommes</option>
@@ -91,7 +101,7 @@ if ($_POST) {
 
     <div>
         <label for="message">Message :</label>
-        <textarea id="message" name="message"><?php if(isset($_POST['message'])) echo $_POST['message'];?></textarea>
+        <textarea id="message" name="message"><?php if (isset($_POST['message'])) echo $_POST['message']; ?></textarea>
         <p><?php if (isset($errors['message1'])) echo $errors['message1'];
             ?></p>
 
