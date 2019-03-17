2f2bb61198f824925a33534dae5123f3a8c0a242
maiwenn99 <maiwenngay99@gmail.com>
2019-03-17 18:19:09 +0100

    create a contactform with php

---
+++ form.php
@@ -0,0 +1,105 @@
+<?php
+if ($_POST) {
+    $errors = array();
+    if (empty($_POST['nom'])) {
+        $errors['nom1'] = "Veuillez renseigner votre nom";
+    }
+
+
+
+    if (empty($_POST['prenom'])) {
+        $errors['prenom1'] = "Veuillez renseigner votre prénom";
+    }
+
+    if (empty($_POST['courriel'])) {
+        $errors['courriel1'] = "Veuillez renseigner votre adresse e-mail";
+    }
+
+
+    if (empty($_POST['message'])) {
+        $errors['message1'] = "Veuillez renseigner votre message";
+    }
+
+
+    if (0 == count($errors)) {
+        echo "Votre message à été envoyez avec succès !";
+        $_POST = "";
+    }
+
+
+}
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+
+
+    <link rel="stylesheet" href="style.css"/>
+
+    <title>Formulaire</title>
+</head>
+<body>
+
+<h1 class="text-center">Contactez-Nous</h1>
+<form action="" method="post">
+    <div>
+        <label for="nom">Nom :</label>
+        <input type="text" id="nom" name="nom" value="<?php if(isset($_POST['nom'])) echo $_POST['nom'];?>"/>
+        <p><?php if (isset($errors['nom1'])) echo $errors['nom1'];
+            ?></p>
+    </div>
+
+
+    <div>
+        <label for="prenom">Prénom :</label>
+        <input type="text" id="prenom" name="prenom" value="<?php if(isset($_POST['prenom'])) echo $_POST['prenom'];?>"/>
+        <p><?php if (isset($errors['prenom1'])) echo $errors['prenom1'];
+            ?></p>
+
+    </div>
+
+    <div>
+        <label for="courriel">Courriel :</label>
+        <input type="email" id="courriel" name="courriel" value="<?php if(isset($_POST['email'])) echo $_POST['email'];?> "/>
+        <p><?php if (isset($errors['courriel1'])) echo $errors['courriel1'];
+            ?></p>
+
+    </div>
+
+    <div>
+        <label for="tel">Téléphone:</label>
+        <input type="tel" pattern="\d+" id="tel" name="tel" value="<?php if(isset($_POST['tel'])) echo $_POST['tel'];?>"/>
+        <p><?php if (isset($errors['tel1'])) echo $errors['tel1'];
+            ?></p>
+    </div>
+
+
+
+    <div>
+        <label for="sujet">Objet:</label>
+        <select name="choix">
+            <option value="choix0">Acheter un MacBook</option>
+            <option value="choix1">Manger des Pommes</option>
+            <option value="choix2">Boire une bière</option>
+            <option value="choix3">Rencontrer Elon Musk</option>
+            <option value="choix4">Vendre ses Chaussons</option>
+        </select>
+    </div>
+
+    <div>
+        <label for="message">Message :</label>
+        <textarea id="message" name="message"><?php if(isset($_POST['message'])) echo $_POST['message'];?></textarea>
+        <p><?php if (isset($errors['message1'])) echo $errors['message1'];
+            ?></p>
+
+    </div>
+
+    <div class="button">
+        <button type="submit">Envoyer</button>
+    </div>
+</form>
+</body>
+</html>
